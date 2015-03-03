decaf3d658303cecc3e6d677880692ed98e0813b
Dreauw <dma.truc@gmail.com>
2015-03-03 09:43:53 +0100

    Popup menu when you right click a packet

--- model/HTTPMessage.java
+++ model/HTTPMessage.java
@@ -1,13 +1,14 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.jnetpcap.packet.PcapPacket;
 
 public class HTTPMessage {
 	private String url;
 	private PcapPacket request;
-	private ArrayList<PcapPacket> responses;
+	private List<PcapPacket> responses;
 	
 	public HTTPMessage(String url, PcapPacket request) {
 		this.url = url;
@@ -15,8 +16,16 @@ public class HTTPMessage {
 		this.responses = new ArrayList<PcapPacket>();
 	}
 	
+	public PcapPacket getRequest() {
+		return request;
+	}
+	
 	public void addResponse(PcapPacket packet) {
 		responses.add(packet);
+	}
+	
+	public List<PcapPacket> getResponses() {
+		return responses;
 	}
 	
 	@Override
--- view/PacketList.java
+++ view/PacketList.java
@@ -1,12 +1,18 @@
 package view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Observable;
 import java.util.Observer;
 
-import javax.swing.DefaultListModel;
 import javax.swing.JList;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 
+import model.HTTPMessage;
 import model.Model;
 import model.ModelMessage;
 
@@ -14,15 +20,86 @@ public class PacketList extends JScrollPane implements Observer {
 	private static final long serialVersionUID = 1L;
 	
 	private JList list;
+	private JPopupMenu popupMenu;
+	private JMenuItem showHTTPReq, showHTTPRes, saveResponse, downloadURL;
 	
 	public PacketList(Model model) {
 		super();
 		model.addObserver(this);
 		
+		generatePopupMenu();
+		
 		list = new JList(model.getHTTPPackets());
 		list.setLayoutOrientation(JList.VERTICAL);
 		
+		list.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseReleased(MouseEvent e) {onMouseClick(e);}
+			@Override
+			public void mousePressed(MouseEvent e) {onMouseClick(e);}
+			@Override
+			public void mouseExited(MouseEvent e) {}
+			@Override
+			public void mouseEntered(MouseEvent e) {}
+			@Override
+			public void mouseClicked(MouseEvent e) {}
+		});
+		
 		setViewportView(list);
+	}
+	
+	/**
+	 * Generate the popup menu (opened on right click)
+	 */
+	private void generatePopupMenu() {
+		popupMenu = new JPopupMenu();
+		popupMenu.add(showHTTPReq = new JMenuItem("Show HTTP Request..."));
+		popupMenu.add(showHTTPRes = new JMenuItem("Show HTTP Response..."));
+		popupMenu.addSeparator();
+		popupMenu.add(saveResponse = new JMenuItem("Save HTTP Response Content..."));
+		popupMenu.add(downloadURL = new JMenuItem("Download the requested URL..."));
+		
+		// Listeners
+		showHTTPReq.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println(getSelectedHTTPMessage().getRequest());
+			}
+		});
+		
+		showHTTPRes.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println(getSelectedHTTPMessage().getResponses().get(0));
+			}
+		});
+	}
+	
+	/**
+	 * Return the selected HTTPMessage
+	 * @return An instance of HTTPMessage
+	 */
+	public HTTPMessage getSelectedHTTPMessage() {
+		return (HTTPMessage) list.getSelectedValue();
+	}
+	
+	/**
+	 * Called when the mouse is pressed/released over an item in the list
+	 */
+	private void onMouseClick(MouseEvent e) {
+		int index = list.locationToIndex(e.getPoint());
+		if (index >= 0 && e.getButton() == MouseEvent.BUTTON3 && e.isPopupTrigger()) {
+			list.setSelectedIndex(index);
+			
+			// If there's no response, disable items that use them
+			if (getSelectedHTTPMessage().getResponses().size() <= 0) {
+				showHTTPRes.setEnabled(false);
+				saveResponse.setEnabled(false);
+			}
+			
+			// Show the popup
+			popupMenu.show(list, e.getX(), e.getY());
+		}
 	}
 
 	@Override
